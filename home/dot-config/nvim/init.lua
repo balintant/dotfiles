d4aa10d29803642276ac91d00d1511b10e55e700
Balint Antal <balint@denv.io>
2025-02-27 09:30:42 +0100

    feat(vim): add basic plugins for nvim, update config

---
+++ home/dot-config/nvim/init.lua
@@ -0,0 +1,74 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load vimrc
+vim.cmd("source ~/.vimrc")
+
+-- Set leader key before lazy
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Load plugins
+require("lazy").setup({
+  -- EditorConfig support
+  {
+    "editorconfig/editorconfig-vim",
+    event = { "BufReadPre", "BufNewFile" },
+  },
+
+  -- nvim-tree file explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- optional, for file icons
+    },
+    version = "*",
+    cmd = "NvimTreeToggle",
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file explorer" },
+    },
+    config = function()
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+      })
+    end,
+  },
+
+  -- Treesitter for better syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "vim", "vimdoc", "bash", "javascript", "typescript", "json" },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
+})
